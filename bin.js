6132978d6406cde739a623315c6baab3e5435fad
Abdul Halim <halim@rokomari.com>
2022-05-19 12:35:16 +0600

    Update package configure file

--- bin.js
+++ bin.js
@@ -32,7 +32,7 @@ try {
 async function main() {
   try {
     console.log("Downloading files...");
-    execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
+    execSync(`git clone ${git_repo} ${projectPath}`);
 
     process.chdir(projectPath);
 
